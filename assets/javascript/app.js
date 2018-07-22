c2c947905a7846b958f66c310e9ff903969725c8
RubyG <rubyrgill@hotmail.com>
2018-07-22 15:20:54 -0400

    Initialize firebase pasted in js, and linked to html

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    //Firebase Database
+    //------------------------
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyByexU0wzg2V7Scr-1aEfDXgZ9diEp1WoU",
+        authDomain: "train-schedule-w7.firebaseapp.com",
+        databaseURL: "https://train-schedule-w7.firebaseio.com",
+        projectId: "train-schedule-w7",
+        storageBucket: "train-schedule-w7.appspot.com",
+        messagingSenderId: "694982066951"
+    };
+
+    firebase.initializeApp(config);
+
+
+
+});
